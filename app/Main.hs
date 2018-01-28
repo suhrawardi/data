1ca7d0b4680d31e18674f654654977bc1c9d3420
jarra <suhrawardi@gmail.com>
2018-01-28 20:34:20 +0100

    Cleaning up a bit

--- app/Main.hs
+++ app/Main.hs
@@ -9,10 +9,9 @@ import System.FilePath.Posix
 main :: IO ()
 main = do
   values <- getArgs
-  print $ values
   let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.csv"
       path = "tmp/" ++ takeFileName url
-  resp <- downloadData url path
+  downloadData url path
   res <- getColumnInCsvFile path (head values)
   print res
   print $ map medianAndMean $ map (map fromIntegral) $ map vowelIndices values
