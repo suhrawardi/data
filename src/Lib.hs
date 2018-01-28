81a16f3a193156a774e6c4cdd43689b2f1503baf
jarra <suhrawardi@gmail.com>
2018-01-28 12:50:35 +0100

    Adding function to calculate median

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,20 @@ module Lib
     ( someFunc
     ) where
 
+import Data.List
+
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+median :: [Double] -> Double
+median [] = 0
+median xs = if oddInLength then
+              middleValue
+            else
+              (middleValue + beforeMiddleValue) / 2
+  where
+    sortedList = sort xs
+    oddInLength = 1 == mod (genericLength xs) 2
+    middle = floor $ (genericLength xs) / 2
+    middleValue = genericIndex sortedList middle
+    beforeMiddleValue = genericIndex sortedList (middle - 1)
